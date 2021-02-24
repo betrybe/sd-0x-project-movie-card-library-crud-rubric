65b5d3cf8857e68a22f6a8cdae6c453aa6e8b6a0
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2021-02-24 08:52:26 -0300

    Fixing linter and breaking testes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import MovieList from './pages/MovieList';
-import MovieDetails from './pages/MovieDetails';
-import NewMovie from './pages/NewMovie';
-import EditMovie from './pages/EditMovie';
-import NotFound from './components/NotFound';
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import MovieList from './pages/MovieList';
+// import MovieDetails from './pages/MovieDetails';
+// import NewMovie from './pages/NewMovie';
+// import EditMovie from './pages/EditMovie';
+// import NotFound from './components/NotFound';
 
 function App() {
   return (
